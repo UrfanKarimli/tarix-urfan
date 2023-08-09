3efb2e88d4c2a4118c92c461597779be794c2941
UrfanKarimli <urfankarim@gmail.com>
2023-08-09 14:45:31 +0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+let a = prompt("ay daxil edin");
+
+switch (a) {
+  case "yanvar":
+    console.log("1-ci ay");
+    console.log("qış");
+    break;
+  case "fevral":
+    console.log("2-ci ay");
+    console.log("qış");
+    break;
+  case "mart":
+    console.log("3-cü ay");
+    console.log("yaz");
+    break;
+  case "aprel":
+    console.log("4-cü ay");
+    console.log("yaz");
+    break;
+  case "may":
+    console.log("5-ci ay");
+    console.log("yaz");
+    break;
+  case "iyun":
+    console.log("6-cı ay");
+    console.log("yay");
+    break;
+  case "iyul":
+    console.log("7-ci ay");
+    console.log("yay");
+    break;
+  case "avqust":
+    console.log("8-ci ay");
+    console.log("yay");
+    break;
+  case "sentyabr":
+    console.log("9-cu ay");
+    console.log("payız");
+    break;
+  case "oktyabr":
+    console.log("10-cu ay");
+    console.log("payız");
+    break;
+  case "noyabr":
+    console.log("11-ci ay");
+    console.log("payız");
+    break;
+  case "dekabr":
+    console.log("12-ci ay");
+    console.log("qış");
+    break;
+  default:
+    console.log("bele ay yoxdur");
+}
+
+
+// if (12==a<<4) {
+//     console.log("qis");
+// }
